f90dd57cc2648cf87ba264e59ae3ebb41517c826
carlosmartin moscosocornejo <carlosmartinmoscosocornejo@gmail.com>
2021-08-24 23:24:19 -0300

    cambuanod datos home

--- src/models/PokemonDatos.js
+++ src/models/PokemonDatos.js
@@ -4,38 +4,21 @@ import queryString from "query-string";
 
 import { Pokemon } from "../models/Pokemon";
 
-export const PokemonDatos = () => {
+export const PokemonDatos = (start) => {
   const [pokemonListaData, setpokemonListadata] = useState([]);
-  const [pokemonData, setpokemondata] = useState({});
 
-  const fetchDataLista = async () => {
+
+  const fetchDataLista = async (start) => {
     const res = await fetch(
-      ` https://pokeapi.co/api/v2/pokemon?offset=0&limit=9`
+      ` https://pokeapi.co/api/v2/pokemon?offset=${start}&limit=10`
     );
-    const data = await res.json(); 
+    const data = await res.json();
     setpokemonListadata(data.results);
-   
-  };
-  const fetchData = async (id) => {
-    try {
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-      const data = await res.json();
-      setpokemondata({
-        imgJuego: data.sprites.front_default,
-        nombre: data.name,
-        experiencia: data.base_experience,
-        hp: data.stats[0].base_stat,
-        ataque: data.stats[1].base_stat,
-        defensa: data.stats[2].base_stat,
-        especial: data.stats[3].base_stat,
-      });
-    } catch (error) {}
   };
 
   useEffect(() => {
-    fetchDataLista();
-    fetchData(1);
-  }, []);
+    fetchDataLista(1 );
+  }, [start]);
 
-  return {pokemonListaData};
+  return { pokemonListaData , fetchDataLista};
 };
